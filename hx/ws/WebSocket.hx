ac87f856f8ec223087d926d5240f2463edf50b13
jefvel <akselkornesjo@gmail.com>
2025-05-10 09:48:17 +0200

    js gate

--- hx/ws/WebSocket.hx
+++ hx/ws/WebSocket.hx
@@ -47,7 +47,11 @@ class WebSocket { // lets use composition so we can intercept send / onmessage a
     }
 
     private function createSocket() {
-        var protocols = _protocols != null ? _protocols : js.undefined;
+        var protocols = #if js 
+            _protocols != null ? _protocols : js.undefined 
+        #else 
+            _protocols
+        #end;
         return new js.html.WebSocket(_url, _protocols);
     }
 
