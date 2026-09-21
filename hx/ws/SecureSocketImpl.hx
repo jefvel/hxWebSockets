9d7ee3bcc9158f2a079ef65d6868feaf505f14b8
jefvel <akselkornesjo@gmail.com>
2026-09-21 23:44:05 +0200

    fix hxnodejs websocket

--- hx/ws/SecureSocketImpl.hx
+++ hx/ws/SecureSocketImpl.hx
@@ -1,3 +1,13 @@
 package hx.ws;
 
+#if hxnodejs
+class SecureSocketImpl extends hx.ws.nodejs.NodeSocket {
+    public var verifyCert: Bool = false;
+    public function setCA(ca) {}
+    public function setHostname(host) {}
+    public function setCertificate(cert, key) {
+    }
+}
+#else
 typedef SecureSocketImpl = sys.ssl.Socket;
+#end
